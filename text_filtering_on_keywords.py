6dd7f9375d0d858f04bc6e9d5054f59f2c2a0956
Rahul Thorat <61758268+RaThorat@users.noreply.github.com>
2022-07-30 13:00:31 +0200

    Update text_filtering_on_keywords.py

--- text_filtering_on_keywords.py
+++ text_filtering_on_keywords.py
@@ -7,19 +7,19 @@ conn = pyodbc.connect('Driver={SQL Server};'
                       'Database=YOUR DATABASE;'
                       'Trusted_Connection=yes;')
 
-
+#Imporing data from server
 df = pd.read_sql_query('YOUR QUERY', conn)
 
 df.columns
 
 df.shape
-#joining all words together from columns title, summary and keywords to create a corpus
+#Your dataframe should have columns named 'Title', 'Summary' and 'Keywords'. Joining all words together from aforementioned columns to create a corpus:
 df["Corpus"] = df["Title"] +" "+ df["Summary"] +" "+ df["Keywords"]
 
 
-#dropping the nan values from summary 
+#dropping the not empty cells from the corpus
 df = df[df['Corpus'].notna()]
-#dropping the punctuation from summary 
+#dropping the punctuation from the corpus 
 import string
 string.punctuation
 def remove_punctuation(txt):
@@ -36,7 +36,7 @@ df['Corpus_clean_tokenized']=df['Corpus_clean'].apply(lambda x:tokenize(x.lower(
 from nltk.corpus import stopwords
 stop_words=stopwords.words('english')
 df['Corpus_nostop']=df['Corpus_clean_tokenized'].apply(lambda x: [item for item in x if item not in stop_words])
-#lemmatization
+#Lemmatization of the corpus
 import nltk
 #nltk.download('wordnet')
 wn=nltk.WordNetLemmatizer()
@@ -46,14 +46,9 @@ def lemmatization(token_text):
 df['Corpus_lemmatized']=df['Corpus_nostop'].apply(lambda x: lemmatization(x))
 
 
-#list from where words to search for
-
-
+#Lists of keywords to search
 main_list = ['YOUR KEYWORD1', 'YOUR KEYWORD2']
 print(main_list)
-
-
-# In[86]:
 
 
 #upper casing and lower casing of words
@@ -61,60 +56,29 @@ main_list_upper = [each_string.upper() for each_string in main_list]
 main_list_lower= [each_string.lower() for each_string in main_list]
 
 
-# In[87]:
-
-
 #combining all the formats of the words to search for
 main_list_1=main_list+main_list_upper+main_list_lower
-
-
-# In[88]:
 
 
 #filtering rows on the basis of words
 df['Corpus_lemmatized_filter']=df['Corpus_lemmatized'].apply(lambda x: [item for item in x if item in main_list_1])
 
-
-# In[89]:
-
-
+#check the dataframe
 df.shape
 
-
-# In[90]:
-
-
 df.head()
-
-
-# In[91]:
-
 
 #dropping the rows on the condition of the column 'Corpus_lemmatized_filter', which do not contain any of the words searched for.
 df1 = df[df['Corpus_lemmatized_filter'].map(lambda d: len(d)) > 0]
 
 
-# In[92]:
-
-
 #check the dataframe
 df1.head()
-
-
-# In[93]:
-
 
 #shape of the dataframe check
 df1.shape
 
-
-# In[94]:
-
-
 df1 = df1.rename(columns={ 'Corpus_lemmatized_filter':'found words'})
-
-
-# In[96]:
 
 #save file to excel
 df1.to_excel('filtering text on keywords.xlsx')
